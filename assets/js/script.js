ecc888bcfeda8b767965d319c37a969f96a1bc35
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-02-27 22:29:03 +0530

    log the updated values.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ const incrementFunction = () => {
     let CurrentValue = document.getElementById("counter-number");
     let IntegerValue = Number(CurrentValue.textContent);
     CurrentValue.textContent = IntegerValue + 1;
+    console.log(CurrentValue.textContent);
 }
 
 const decrementFunction = () => {
-    console.log(`decrement logged!`)
+    console.log(`decrement logged!`);
     let CurrentValue = document.getElementById("counter-number");
     let IntegerValue = Number(CurrentValue.textContent);
     CurrentValue.textContent = IntegerValue - 1;
+    console.log(CurrentValue.textContent);
 }
