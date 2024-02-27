24e2768ec6f70c0236d27934b052de14146e247a
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-02-27 20:24:55 +0530

    Increment_Decrement_Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 const incrementFunction = () => {
     console.log(`increment logged!`)
+    let CurrentValue = document.getElementById("counter-number");
+    let IntegerValue = Number(CurrentValue.textContent);
+    CurrentValue.textContent = IntegerValue + 1;
 }
 
 const decrementFunction = () => {
     console.log(`decrement logged!`)
+    let CurrentValue = document.getElementById("counter-number");
+    let IntegerValue = Number(CurrentValue.textContent);
+    CurrentValue.textContent = IntegerValue - 1;
 }
